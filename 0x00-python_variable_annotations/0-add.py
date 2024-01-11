33a025fdec8dab7627114ce0d1c01592320eb364
AnaduCG <godzionwin@gmail.com>
2024-01-11 11:35:05 +0100

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+"""annoted function"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    annoted function that accept two
+    floats arguments and return their sum
+    """
+    return a + b
