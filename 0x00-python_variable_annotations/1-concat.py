4fb5bfd9d0042f682c6b09ee0cebc57b7c044933
AnaduCG <godzionwin@gmail.com>
2024-01-11 12:04:08 +0100

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""a type-annotated function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    type-annotated function that
+    accepts string arguments and returns
+    them caoncatenated
+    """
+    return str1 + str2
