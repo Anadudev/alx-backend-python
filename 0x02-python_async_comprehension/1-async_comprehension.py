4aec58e4311b86d2141799b7ba4650c011780b92
AnaduCG <godzionwin@gmail.com>
2024-01-17 10:00:33 +0100

    update: 1-async_comprehension.py function return type hint correction

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> None:
+async def async_comprehension() -> List[float]:
     """
     a coroutine called async_comprehension that
     takes no arguments it collect 10 random numbers
