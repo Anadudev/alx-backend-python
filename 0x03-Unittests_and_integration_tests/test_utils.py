9891f791fea30394f609094a0bf1a56443964349
AnaduCG <godzionwin@gmail.com>
2024-02-05 02:24:30 +0100

    Update: formatting

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,9 +2,9 @@
 """_summary_
 """
 import unittest
+from unittest.mock import patch
 from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
-from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -13,46 +13,54 @@ class TestAccessNestedMap(unittest.TestCase):
     Args:
         unittest (_type_): _description_
     """
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-        ])
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
     def test_access_nested_map(self, nested_map, path, output):
         """_summary_"""
         self.assertEqual(access_nested_map(nested_map, path), output)
 
-    @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b"))
-        ])
+    @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
     def test_access_nested_map_exception(self, nested_map, path):
-        """ testing for function assertion """
+        """testing for function assertion"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
-    """ Mock http calls """
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False})
-        ])
-    @patch('test_utils.get_json')
+    """Mock http calls"""
+
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+    )
+    @patch("test_utils.get_json")
     def test_get_json(self, url, payload, mock_get):
-        """ testing return data of get_json method"""
+        """testing return data of get_json method"""
         mock_get.return_value = payload
         result = get_json(url)
         self.assertEqual(result, payload)
 
 
 class TestMemoize(unittest.TestCase):
-    """ tesing the memoize method """
+    """testing the memoize method"""
+
     class TestClass:
         def a_method(self):
             return 42
 
     @memoize
     def a_property(self):
-        """ __summery__ """
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return self.a_method()
